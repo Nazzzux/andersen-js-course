077be93fc4b3cb53957049f32088955447add70a
Nazzzux <alex.rochnyak@gmail.com>
2022-06-15 18:17:32 +0300

    bug fix

--- lesson1.js
+++ lesson1.js
@@ -1,9 +1,11 @@
+const ERROR_MESSAGE = 'Некорректный ввод!';
+
 // 1
 let userNumber = prompt('Введите, пожалуйста, конвертируемое число:'),
     userBase = prompt('Введите, пожалуйста,  основание системы счисления:');
 
 function toNumeralSystem(num, base) {
-  return !(isNaN(num) || isNaN(base)) ? (num * 1).toString(base * 1) : 'Некорректный ввод!';
+  return (isNaN(num) || isNaN(base)) ? ERROR_MESSAGE :  Number(num).toString(Number(base));
 };
 
 console.log(toNumeralSystem(userNumber, userBase));
@@ -14,10 +16,10 @@ let userFirstNumber = prompt('Введите, пожалуйста, первое
     userSecondNumber = prompt('Введите, пожалуйста,  второе число:');
 
 function sumAndQuotient(first, second) {
-  if ( !(isNaN(first) || isNaN(second)) ) {
-    return `Ответ: ${(first * 1) + (second * 1)}, ${first / second}`;
+  if ( isNaN(first) || isNaN(second) ) {
+    return ERROR_MESSAGE;
   } else {
-    return 'Некорректный ввод!'
+    return `Ответ: ${Number(first) + Number(second)}, ${first / second}`;
   }
 };
 
