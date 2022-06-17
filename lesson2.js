66b6243e28c06e11528b411876ad9f858bc9c631
Nazzzux <alex.rochnyak@gmail.com>
2022-06-17 09:01:13 +0300

    finished first task in lesson#2

---
+++ lesson2.js
@@ -0,0 +1,16 @@
+// 1
+
+const deepClone = (object) => {
+
+  if (typeof(object) !== 'object' || object === null) {
+    return object;
+  }
+
+  const result = Array.isArray(object) ? [] : {};
+
+  for (let key in object) {
+    result[key] = deepClone(object[key])
+  }
+
+  return result;
+}
